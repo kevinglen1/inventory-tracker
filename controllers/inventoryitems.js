d5b6653daaec0e49c3bb5da8002801c03fbb8f4f
Kevin <kevinpeterglen@gmail.com>
2022-05-18 01:07:48 -0400

    added edit page

--- controllers/inventoryitems.js
+++ controllers/inventoryitems.js
@@ -4,6 +4,8 @@ module.exports = {
     new: newInventoryItem,
     create,
     delItem,
+    renderEditPage,
+    editItem,
 };
 
 function newInventoryItem(req, res) {
@@ -20,12 +22,27 @@ function create(req, res) {
 }
 
 function delItem(req, res) {
-    let inventory = req.body;
-    console.log(req.body);
-    inventory.deleteOne({ itemName: item }, function (err) {
+    Inventory.deleteOne({ _id: req.params.id }, function (err) {
         if (err) return handleError(err);
-        // deleted at most one tank document
       });
       res.redirect('new');
 }
 
+function editItem(req, res) {
+    let filter = { _id: req.params.id};
+    let update = { _id: req.params.itemName};
+
+    Inventory.findOneAndUpdate(filter, update, {
+        returnOriginal: false
+    })
+}
+
+function renderEditPage(req, res) {
+    Inventory.findById(req.params.id, function (err, item) {
+        res.render('edit', { item })
+    })
+
+    // console.log('req.inventories =',req.inventories);
+    // console.log('req.params.id =',req.params.id);
+    // res.render('edit', { item: req.inventories(req.params.id)})
+}
--- routes/inventoryitems.js
+++ routes/inventoryitems.js
@@ -5,5 +5,9 @@ const inventoryItemsCtrl = require('../controllers/inventoryitems');
 /* GET inventoryitems listing. */
 router.get('/new', inventoryItemsCtrl.new);
 router.post('/', inventoryItemsCtrl.create);
+router.delete("/:id", inventoryItemsCtrl.delItem)
+router.put("/:id", inventoryItemsCtrl.editItem)
+router.get("/:id", inventoryItemsCtrl.renderEditPage)
+
 
 module.exports = router;
