fee6d4ef6cfb3c1e1b4665f16520b18562de5d40
Kevin <kevinpeterglen@gmail.com>
2022-05-22 22:44:59 -0400

    added database

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1/inventory-tracker');
+mongoose.connect('mongodb+srv://Kevin:blanket99@cluster0.lt2j6.mongodb.net/?retryWrites=true&w=majority');
 
 // shortcut to mongoose.connection object
 const db = mongoose.connection;
